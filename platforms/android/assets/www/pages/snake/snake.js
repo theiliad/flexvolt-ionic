0c813b7cce4f25848a313de563b5fdcf518c14f1
The Iliad <iliadmoosavi@gmail.com>
2018-03-15 19:41:42 -0400

    Removed gitignore

---
+++ platforms/android/assets/www/pages/snake/snake.js
@@ -0,0 +1,41 @@
+(function() {
+    'use strict'
+
+    angular.module('flexvolt.snake', [])
+    .controller('SnakeCtrl', ['$stateParams', '$scope', '$state', 'xyDot', '$ionicPopover', '$ionicModal', 'customPopover',
+    function($stateParams, $scope, $state, xyDot, $ionicPopover, $ionicModal, customPopover) {
+        var currentUrl = $state.current.url;
+        console.log('currentUrl = '+currentUrl);
+        $scope.demo = $stateParams.demo;
+
+        customPopover.add($ionicPopover, $scope, 'popover', 'pages/snake/settings.html');
+        // customPopover.add($ionicPopover, $scope, 'helpover','pages/snake/help.html');
+        customPopover.addHelp($ionicModal, $scope, 'helpModal','pages/snake/snake-help.html');
+
+        var afID;
+
+        var frameCounts = 0;
+        var speed = 4;
+        var x = 128, y = 128;
+//
+//        xyDot.init('#snakeWindow');
+
+//        function updateAnimate(){
+//            if ($scope.updating) return;
+//        }
+//
+//        function paintStep(timestamp){
+//            if ($state.current.url === currentUrl){
+//                afID = window.requestAnimationFrame(paintStep);
+//                frameCounts++;
+//                if (frameCounts > 5){
+//                    frameCounts = 0;
+//                    updateAnimate();
+//                }
+//            }
+//        }
+//
+//        paintStep();
+    }])
+
+}())
---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,66 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-bluetooth-serial.bluetoothSerial",
+        "file": "plugins/cordova-plugin-bluetooth-serial/www/bluetoothSerial.js",
+        "pluginId": "cordova-plugin-bluetooth-serial",
+        "clobbers": [
+            "window.bluetoothSerial"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-ble-central.ble",
+        "file": "plugins/cordova-plugin-ble-central/www/ble.js",
+        "pluginId": "cordova-plugin-ble-central",
+        "clobbers": [
+            "ble"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "ionic-plugin-keyboard.keyboard",
+        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-bluetooth-serial": "0.4.7",
+    "cordova-plugin-device": "1.1.7",
+    "cordova-plugin-compat": "1.2.0",
+    "cordova-plugin-ble-central": "1.1.4",
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-statusbar": "2.2.3",
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-splashscreen": "4.0.3"
+};
+// BOTTOM OF METADATA
+});
